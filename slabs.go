0b484f7dca69433f55a01c75029d716d04791daa
watson.jiang <watson.jiang@gmail.com>
2014-05-12 23:24:46 +0800

    add split_slab_page_into_freelist

--- slabs.go
+++ slabs.go
@@ -129,7 +129,7 @@ func slabs_preallocate(maxslabs uint) {
       if prealloc > maxslabs {
          return
       }
-      if DoSlabsNewSlab(i) == 0 {
+      if do_slabs_new_slab(i) == 0 {
          fmt.Println("Error while preallocating slab memory")
          os.Exit(1)
       }
@@ -137,22 +137,35 @@ func slabs_preallocate(maxslabs uint) {
 }
 
 func grow_slab_list(id uint) {
-  p := &slabclass[id]
-  if p.slabs == p.list_size {
-     var new_size uint 
-     if p.list_size == 0 {
-        new_size = 16
-     }else {
-        new_size = p.list_size * 2
-     }
-     new_list := uintptr(C.realloc(unsafe.Pointer(p.slab_list), C.size_t(new_size*uint(unsafe.Sizeof(p.slab_list)))))
-     p.list_size = new_size
-     p.slab_list = new_list
-  }
+   p := &slabclass[id]
+   if p.slabs == p.list_size {
+      var new_size uint 
+      if p.list_size == 0 {
+         new_size = 16
+      }else {
+         new_size = p.list_size * 2
+      }
+      new_list := uintptr(C.realloc(unsafe.Pointer(p.slab_list), C.size_t(new_size*uint(unsafe.Sizeof(p.slab_list)))))
+      p.list_size = new_size
+      p.slab_list = new_list
+   }
 }
 
-func DoSlabsNewSlab(id uint) int {
+func split_slab_page_into_freelist(ptr uintptr, id uint) {
+   p := &slabclass[id]
+   tmp := unsafe.Pointer(ptr)
+   var x uint64 = 0
+   for x=0;x<p.perslab;x++ {
+       do_slabs_free(ptr, 0, id)
+       tmp = unsafe.Pointer(&C.GoBytes(tmp, 1024*1024)[p.size]);
+   }
+}
+
+func do_slabs_new_slab(id uint) int {
    //p := &slabclass[id]
    //len := 
   return 0
+}
+
+func do_slabs_free(ptr uintptr, size size_t, id uint) {
 }

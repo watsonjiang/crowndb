0382f2147e1e7a724701c636667dbd3d91826133
watson.jiang <watson.jiang@gmail.com>
2014-05-18 19:24:37 +0800

    slabs.go finished.

--- slabs.go
+++ slabs.go
@@ -5,7 +5,8 @@ package mempool
 import (
       "fmt"
       "unsafe"
-      "os"
+      "sync"
+      "container/list"
       )
 //#include <stdlib.h>
 //#include <string.h>
@@ -27,21 +28,19 @@ type slabclass_t struct {
 }
 
 const (
+   SLAB_SIZE = 1024 * 1024
+   CHUNK_SIZE = 32
    CHUNK_ALIGN_BYTES = 8
-   ITEM_SIZE_MAX = 1024 * 1024
-   MAX_NUMBER_OF_SLAB_CLASSES = 256
+   MAX_NUM_OF_SLAB_CLS = 256
 )
 
-var slabclass [MAX_NUMBER_OF_SLAB_CLASSES]slabclass_t
+var slabclass [MAX_NUM_OF_SLAB_CLS]slabclass_t
 var power_smallest uint = 0      /*the smallest slabcalss id*/
 var power_largest uint = 0       /*the largest slabclass id*/
 var mem_limit size_t
-var mem_malloced size_t
-
-/* used in prealloc mode */
-var mem_base unsafe.Pointer
-var mem_current unsafe.Pointer
-var mem_avail size_t
+var mem_alloced size_t
+var free_slab_list list.List
+var slabs_lock sync.Mutex
 
 /* Figures out which slab class is required to store an item
    of given size
@@ -53,7 +52,7 @@ func slabs_clsid(size size_t) uint {
    res := uint(0)
    for ;size > slabclass[res].size; {
       res++
-      if res ==  MAX_NUMBER_OF_SLAB_CLASSES {
+      if res ==  MAX_NUM_OF_SLAB_CLS {
          return 0
       }
    }
@@ -62,69 +61,104 @@ func slabs_clsid(size size_t) uint {
 
 /* Determines the chunk sizes and initializes the slab calss */
 func SlabInit(limit size_t, factor float32, prealloc bool) {
-   item := item_t{} 
-   size := size_t(unsafe.Sizeof(item))
+   item := item_t{}
+   size := size_t(unsafe.Sizeof(item)) + CHUNK_SIZE
    mem_limit = limit
 
    if prealloc {
-      mem_base = C.malloc(C.size_t(mem_limit))
-      if mem_base != nil {
-         mem_current = mem_base
-         mem_avail = mem_limit
-      } else {
-         fmt.Println("Warning: Failed to allocate required memory in",
-                     "one large chunk.")
-         os.Exit(1)
+      /* prealloc 100 free slabs */
+      num_slabs := limit / size_t(SLAB_SIZE)
+      if limit % SLAB_SIZE != 0 {
+         num_slabs++
+      }
+      for i:=size_t(0); i<num_slabs; i++ {
+         tmp := C.malloc(C.size_t(SLAB_SIZE))
+         free_slab_list.PushBack(tmp)
+         mem_alloced += SLAB_SIZE
       }
    }
 
-   C.memset(unsafe.Pointer(&slabclass[0]), 
+   C.memset(unsafe.Pointer(&slabclass[0]),
             0, C.size_t(unsafe.Sizeof(slabclass)))
-   var i uint
-   for i=1;i<MAX_NUMBER_OF_SLAB_CLASSES;i++ {
+
+   for i:=uint(0);i<MAX_NUM_OF_SLAB_CLS;i++ {
       /*make sure items are always n-byte aligned */
       if size % CHUNK_ALIGN_BYTES != 0 {
          size += CHUNK_ALIGN_BYTES - (size % CHUNK_ALIGN_BYTES)
       }
-      slabclass[i].size = size
-      slabclass[i].perslab = size_t(ITEM_SIZE_MAX) / slabclass[i].size
-      size = size_t(float64(size) * float64(factor))
-      if size > ITEM_SIZE_MAX {
+      if size > SLAB_SIZE {
+         /*max chunk size is SLAB_SIZE*/
+         slabclass[i].size = 0
+         slabclass[i].perslab = 0   //use 0 as end flag
+         power_largest = i-1
          break
       }
-      fmt.Println("slab class", i, "chunk size", slabclass[i].size,
-                  "perslab", slabclass[i].perslab)
+      slabclass[i].size = size
+      slabclass[i].perslab = size_t(SLAB_SIZE) / slabclass[i].size
+      size = size_t(float64(size) * float64(factor))
    }
-   power_largest = i
-   slabclass[i].size = ITEM_SIZE_MAX
-   slabclass[i].perslab = 1
-   fmt.Println("slab class", i, "chunk size", slabclass[i].size,
-               "perslab", slabclass[i].perslab)
 
-   if prealloc {
-      slabs_preallocate(MAX_NUMBER_OF_SLAB_CLASSES)
+   // may take 6x seconds to init, :(
+   //if prealloc {
+   //   slabs_preallocate()
+   //}
+}
+
+func SlabsAlloc(size size_t, id uint) unsafe.Pointer {
+   slabs_lock.Lock()
+   defer slabs_lock.Unlock()
+
+   return do_slabs_alloc(size, id)
+}
+
+func SlabsFree(ptr unsafe.Pointer, size size_t, id uint) {
+   slabs_lock.Lock()
+   defer slabs_lock.Unlock()
+
+   do_slabs_free(ptr, size, id)
+}
+
+/* dump the slabclass table for debug purpose. */
+func slabs_cls_info_dump(){
+   for i:=uint(0);i<power_largest;i++ {
+      fmt.Println("class", i, "size", slabclass[i].size)
    }
 }
 
-func slabs_preallocate(maxslabs uint) {
+func do_slabs_alloc(size size_t, id uint) unsafe.Pointer {
+   if (id < power_smallest && id > power_largest) {
+      panic("Invalid class id")
+   }
+   var ret unsafe.Pointer
+   p:=&slabclass[id]
+   if p.sl_curr == 0 {
+      do_slabs_newslab(id)
+   }
+
+   if p.sl_curr != 0 {
+      it := p.slots;
+      p.slots = it.next
+      if it.next != nil {
+         it.next.prev = nil
+      }
+      p.sl_curr--
+      ret = unsafe.Pointer(it)
+   }
+   p.requested += size
+   return ret
+}
+
+func slabs_preallocate() {
    /* pre-allocate a 1mb slab in every size class.*/
-   var prealloc uint = 1
-   var i uint
-   for i=1;i<=MAX_NUMBER_OF_SLAB_CLASSES;i++ {
-      if prealloc > maxslabs {
-         return
-      }
-      if do_slabs_newslab(i) == 0 {
-         fmt.Println("Error while preallocating slab memory")
-         os.Exit(1)
-      }
+   for i:=uint(0);i<=power_largest;i++ {
+      do_slabs_newslab(i)
    }
 }
 
 func grow_slab_list(id uint) {
    p := &slabclass[id]
    if p.slabs == p.list_size {
-      var new_size uint 
+      var new_size uint
       if p.list_size == 0 {
          new_size = 16
       }else {
@@ -141,51 +175,45 @@ func split_slab_page_into_freelist(ptr unsafe.Pointer, id uint) {
    p := &slabclass[id]
    tmp := ptr
    var x size_t = 0
+   fmt.Println("p.id", id, "p.perslab", p.perslab)
    for x=0;x<p.perslab;x++ {
-       do_slabs_free(ptr, 0, id)
-       tmp = unsafe.Pointer(&C.GoBytes(tmp, 1024*1024)[p.size]);
+       do_slabs_free(tmp, 0, id)
+       tmp = unsafe.Pointer(&C.GoBytes(tmp, SLAB_SIZE)[p.size]);
    }
 }
 
-func memory_allocate(size size_t) unsafe.Pointer {
-   var ret unsafe.Pointer = nil
-   if mem_base == nil {
-      /* not in prealloc mode, using system malloc. */
-      ret = C.malloc(C.size_t(size))
-   } else {
-      ret = mem_current
-      if size > mem_avail {
-         panic("Not enough memory")
-         return nil
+func memory_allocate_newslab() unsafe.Pointer {
+   fmt.Println("!!!!!!free_slab_list len", free_slab_list, free_slab_list.Len())
+   if free_slab_list.Len() == 0 {
+      /* no available slab in free slab list */
+      mem_alloced += size_t(SLAB_SIZE)
+      if mem_alloced > mem_limit {
+         panic("Out of memory")
       }
-      /* mem_current pointer must be aligned!!! */
-      if size % CHUNK_ALIGN_BYTES != 0 {
-         size += CHUNK_ALIGN_BYTES - (size % CHUNK_ALIGN_BYTES)
-      }
-
-      mem_current = unsafe.Pointer(&C.GoBytes(mem_current, 255)[size])
-      if size < mem_avail {
-         mem_avail -= size
-      }else{
-         mem_avail = 0
-      }
+      tmp := C.malloc(C.size_t(SLAB_SIZE))
+      fmt.Println("!!!!!malloc mem", tmp)
+      free_slab_list.PushBack(tmp)
+      mem_alloced += SLAB_SIZE
    }
+   ret := free_slab_list.Front().Value.(unsafe.Pointer)
+   fmt.Println("newslab", ret) 
    return ret
 }
 
-func do_slabs_newslab(id uint) int {
+/* new slab from system memory */
+func do_slabs_newslab(id uint) {
    p := &slabclass[id]
-   len := p.size * p.perslab
+   if p.slabs >= p.list_size {
+      grow_slab_list(id)
+   }
+   ptr := memory_allocate_newslab()
 
-   ptr:=memory_allocate(len)
-
-   C.memset(ptr, 0, C.size_t(len))
+   C.memset(ptr, 0, C.size_t(SLAB_SIZE))
    split_slab_page_into_freelist(ptr, id)
 
    p.slabs++
    tmp := (*[1<<16]unsafe.Pointer)(p.slab_list)
    tmp[p.slabs] = ptr
-   return 0
 }
 
 func do_slabs_free(ptr unsafe.Pointer, size size_t, id uint) {
--- slabs_test.go
+++ slabs_test.go
@@ -3,21 +3,36 @@ package mempool
 import (
    "fmt"
    "testing"
-   "unsafe"
+   //"unsafe"
 )
 
+var _slabs_inited bool = false
+
+
+func init_slabs() {
+   if !_slabs_inited {
+      SlabInit(1024 * 1024, 1.2, true)
+      slabs_cls_info_dump()
+      _slabs_inited = true
+   }
+}
+
 func Test_slabs_clsid(t *testing.T) {
-   item := item_t{}
-   fmt.Println("sizeof item", unsafe.Sizeof(item))
+   init_slabs()
+   fmt.Println("clsid for 32 bytes:", slabs_clsid(32))
+   fmt.Println("clsid for 64 bytes:", slabs_clsid(64))
+   fmt.Println("clsid for 128 bytes:", slabs_clsid(128))
 }
 
-func Test_SlabInit(t *testing.T) {
-   SlabInit(1024*1024, 1.2, false)
-   fmt.Println("mem_current", mem_current, "mem_avail", mem_avail)
-}
-
-func Test_demo(t *testing.T) {
-   fmt.Println("sizeof slabclass_t", unsafe.Sizeof(slabclass[0]))
-   fmt.Println("sizeof slabclass", unsafe.Sizeof(slabclass))
+func Test_SlabsAllocFree(t *testing.T) {
+   init_slabs()
+   p1 := SlabsAlloc(32, 0)
+   p2 := SlabsAlloc(128, 1)
+   fmt.Println("clsid", 0, "requested", slabclass[0].requested)
+   fmt.Println("clsid", 1, "requested", slabclass[1].requested)
+   SlabsFree(p1, 32, 0)
+   SlabsFree(p2, 128, 1)
+   fmt.Println("clsid", 0, "requested", slabclass[0].requested)
+   fmt.Println("clsid", 1, "requested", slabclass[1].requested)
 }
 
